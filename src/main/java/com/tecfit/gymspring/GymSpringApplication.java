9a5d9b36e8d95b7e31e4c3d90999bee04a34ea5c
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-09-27 21:07:38 -0500

    create-project

---
+++ src/main/java/com/tecfit/gymspring/GymSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tecfit.gymspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tecfit/gymspring/GymSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecfit.gymspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

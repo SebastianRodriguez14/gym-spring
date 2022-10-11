1b6996821b42d63279902f0d3d42be821ee8d4cd
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-10-10 23:01:57 -0500

    add services for check membership and update membership in user

--- src/main/java/com/tecfit/service/MembershipService.java
+++ src/main/java/com/tecfit/service/MembershipService.java
@@ -3,8 +3,12 @@ package com.tecfit.service;
 import com.tecfit.model.Membership;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface MembershipService {
     Collection<Membership> findAll();
     Membership findById(Integer id);
+
+    Optional<Membership> checkMembershipByUser(Integer id_user);
+
 }

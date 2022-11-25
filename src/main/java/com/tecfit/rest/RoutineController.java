a3b3e6c304bd373f45c0cb45a2ef6cab9a03b189
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-11-25 01:27:05 -0500

    add cross origin for routine

--- src/main/java/com/tecfit/rest/RoutineController.java
+++ src/main/java/com/tecfit/rest/RoutineController.java
@@ -6,14 +6,12 @@ import com.tecfit.model.custom.ExerciseCustom;
 import com.tecfit.model.custom.RoutineCustom;
 import com.tecfit.service.RoutineService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("routine")
 public class RoutineController {
 
